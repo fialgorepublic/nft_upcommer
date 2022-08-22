da7bcaa629414ad91e9432daabb99b4c5579fa86
John Tambi <jtambi@experfy.com>
2022-08-22 18:38:16 +0500

    integrated admin panel of active admin.

---
+++ app/admin/nft_events.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register NftEvent do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :blockchain, :collection_count, :trait_count, :sale_price, :pre_sale_price, :pre_sale_date, :public_sale_date, :tags, :twitter_link, :discord_link, :website_link, :market_place_link, :email, :specific_request, :plan_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :blockchain, :collection_count, :trait_count, :sale_price, :pre_sale_price, :pre_sale_date, :public_sale_date, :tags, :twitter_link, :discord_link, :website_link, :market_place_link, :email, :specific_request, :plan_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
