9936f5cfae30c716f2b647b49f10a797e88123a3
“Fahad <fahad.ishfaq@algorepublic.com>
2022-09-01 14:56:11 +0500

    update approve nft

--- app/admin/nft_events.rb
+++ app/admin/nft_events.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register NftEvent do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-   permit_params :name, :description, :blockchain, :collection_count, :trait_count, :sale_price, :pre_sale_price, :pre_sale_date, :public_sale_date, :twitter_link, :discord_link, :website_link, :market_place_link, :email, :specific_request, :plan_id
+   permit_params :name, :description, :blockchain, :collection_count, :trait_count, :sale_price, :pre_sale_price, :pre_sale_date, :public_sale_date, :twitter_link, :discord_link, :website_link, :market_place_link, :email, :specific_request, :plan_id, :approved
   #
   # or
   #
