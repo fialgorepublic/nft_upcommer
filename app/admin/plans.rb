0242db01675e9224eb4aab579a9e31250f50fb70
“Fahad <fahad.ishfaq@algorepublic.com>
2022-09-02 16:33:31 +0500

    set value of description list in admin panel

--- app/admin/plans.rb
+++ app/admin/plans.rb
@@ -1,18 +1,16 @@
 ActiveAdmin.register Plan do
+  permit_params :name, :plan_type, :day_price, :week_price, :daily_text, :weekly_text, :description_list
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :name, :description_list, :plan_type, :day_price, :week_price, :daily_text, :weekly_text
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :day_price, :week_price]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form do |f|
+    f.inputs "Callout" do
+      f.input :name
+      f.input :plan_type
+      f.input :day_price
+      f.input :week_price
+      f.input :daily_text
+      f.input :weekly_text
+      f.input :description_list, input_html: {value: "#{f.object.description_list.any? ? f.object.description_list.map(&:inspect).join(', ').delete('') : ''}", disabled: true}
+      f.actions
+    end
+  end
 end
