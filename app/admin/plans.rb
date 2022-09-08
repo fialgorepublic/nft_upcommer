2b876d7792bed915008f08c40c025cb23d2de51a
“Fahad <fahad.ishfaq@algorepublic.com>
2022-09-08 14:45:09 +0500

    minor fix in plan activeadmin

--- app/admin/plans.rb
+++ app/admin/plans.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Plan do
-  permit_params :name, :plan_type, :day_price, :week_price, :daily_text, :image, :weekly_text, :description_list
+  permit_params :name, :plan_type, :day_price, :week_price, :daily_text, :image, :weekly_text, description_list: []
 
   show do
     attributes_table do
@@ -19,14 +19,14 @@ ActiveAdmin.register Plan do
 
   
   form do |f|
-    f.inputs "Callout" do
+    f.inputs "Plan" do
       f.input :name
       f.input :plan_type
       f.input :day_price
       f.input :week_price
       f.input :daily_text
       f.input :weekly_text
-      f.input :description_list, input_html: {value: "#{f.object.description_list.any? ? f.object.description_list.map(&:inspect).join(', ').delete('') : ''}", disabled: true}
+      f.input :description_list
       f.input :image, as: :file
       f.actions
     end
